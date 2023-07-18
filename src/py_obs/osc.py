b40be9b94d50345fd394858b303fa64e003d5ef0
Daniel Mach <daniel.mach@suse.com>
2023-07-18 22:45:22 +0200

    Implement signature (SSH) auth
    Co-authored-by: Dan Čermák <dcermak@suse.com>

--- src/py_obs/osc.py
+++ src/py_obs/osc.py
@@ -1,6 +1,9 @@
 import aiohttp
 import dataclasses
 import os
+import re
+import subprocess
+import time
 import typing
 
 from py_obs.logger import LOGGER
@@ -17,7 +20,8 @@ class ObsException(aiohttp.ClientResponseError):
 @dataclasses.dataclass
 class Osc:
     username: str
-    password: str
+    password: str | None = None
+    ssh_public_key: str | None = None
 
     api_url: str = "https://api.opensuse.org/"
 
@@ -29,9 +33,14 @@ class Osc:
     def from_env() -> "Osc":
         if not (username := os.getenv("OSC_USER")):
             raise ValueError("environment variable OSC_USER is not set")
-        if not (password := os.getenv("OSC_PASSWORD")):
-            raise ValueError("environment variable OSC_PASSWORD is not set")
-        return Osc(username=username, password=password)
+        ssh_public_key = None
+        if not (password := os.getenv("OSC_PASSWORD")) and not (
+            ssh_public_key := os.getenv("OSC_SSH_PUBKEY")
+        ):
+            raise ValueError(
+                "environment variable OSC_PASSWORD or OSC_SSH_PUBKEY is not set"
+            )
+        return Osc(username=username, password=password, ssh_public_key=ssh_public_key)
 
     async def api_request(
         self,
@@ -55,12 +64,59 @@ class Osc:
             raise ObsException(**cre_exc.__dict__) from cre_exc
 
     def __post_init__(self) -> None:
+        # https://github.com/openSUSE/open-build-service/issues/13737
+        headers = {"Accept": "application/xml; charset=utf-8"}
+        auth = None
+
+        if self.password:
+            auth = aiohttp.BasicAuth(login=self.username, password=self.password)
+
+        elif self.ssh_public_key:
+            realm = "Use your developer account"
+            # Signature realm="Use your developer account",headers="(created)"
+
+            now = int(time.time())
+            data = f"(created): {now}"
+            cmd = [
+                "ssh-keygen",
+                "-Y",
+                "sign",
+                "-f",
+                self.ssh_public_key,
+                "-n",
+                realm,
+                "-q",
+            ]
+            proc = subprocess.Popen(
+                cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, encoding="utf-8"
+            )
+            stdout, _ = proc.communicate(data)
+
+            match = re.match(
+                r"\A-----BEGIN SSH SIGNATURE-----\n(.*)\n-----END SSH SIGNATURE-----",
+                stdout,
+                re.S,
+            )
+            if not match or not match.group(1):
+                raise ValueError(f"ssh-keygen output did not match: {stdout}")
+
+            # headers must not contain newlines; removing them makes no
+            # difference in base64 encoded text
+            sig = match.group(1).replace("\n", "")
+
+            headers["Authorization"] = (
+                f'Signature keyId="{self.username}",algorithm="ssh",'
+                f'headers="(created)",created={now},signature="{sig}"'
+            )
+
+        else:
+            raise ValueError("password or ssh_public_key parameters must be provided")
+
         self._session = aiohttp.ClientSession(
-            auth=aiohttp.BasicAuth(login=self.username, password=self.password),
+            auth=auth,
             raise_for_status=True,
             base_url=self.api_url,
-            # https://github.com/openSUSE/open-build-service/issues/13737
-            headers={"Accept": "application/xml; charset=utf-8"},
+            headers=headers,
         )
 
     async def teardown(self) -> None:
